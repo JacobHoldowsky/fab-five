187afb9638bfe94d28753a1862a83c3a334bc708
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-04-05 21:37:25 -0400

    fixed bug where best player was not rendering on team detail

--- react-app/src/components/TeamDetail.js
+++ react-app/src/components/TeamDetail.js
@@ -7,38 +7,40 @@ import './TeamDetail.css'
 
 const TeamDetail = () => {
     const dispatch = useDispatch()
-    const teams = useSelector((state) => Object.values(state.teams))
-    const user = useSelector((state) => state.session.user)
+    const teams = useSelector((state) => state.teams)
     const { teamId } = useParams()
+    const user = useSelector((state) => state.session.user)
     const team = teams[teamId]
-    console.log(team)
+    console.log('teams', teams)
+    console.log('team', team)
 
     const bestPlayer = team?.players[0]
+    console.log('bestplayer', bestPlayer)
     const remainingPlayers = team?.players.slice(1)
     const remainingPlayers1 = team?.players.slice(1, 3)
-    const remainingPlayers2 = team?.players.slice(2)
+    const remainingPlayers2 = team?.players.slice(3)
     console.log(remainingPlayers)
 
     useEffect(() => {
         dispatch(getAllFollowedTeams(user.id))
-    }, [dispatch, user.id])
+    }, [dispatch, user.id, teamId]);
 
     return (
         <div className='team-detail-page'>
             <div>
-                <img className='best-player-img' src={bestPlayer?.headshot_src} alt="" />
+                <img className='best-player-team-detail-img' src={bestPlayer?.headshot_src} alt="" />
             </div>
             <div>
                 {remainingPlayers1?.map((player) => (
                     <div key={player.id}>
-                        <img className='player-img' src={player?.headshot_src} alt="" />
+                        <img className='player-img' src={player.headshot_src} alt="" />
                     </div>
                 ))}
             </div>
             <div>
                 {remainingPlayers2?.map((player) => (
                     <div key={player.id}>
-                        <img className='player-img' src={player?.headshot_src} alt="" />
+                        <img className='player-img' src={player.headshot_src} alt="" />
                     </div>
                 ))}
             </div>
